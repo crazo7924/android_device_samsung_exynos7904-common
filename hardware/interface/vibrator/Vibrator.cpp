066bdaaf00a97a74ad06428536037b67f096a794
Bharat Dev Burman <bharat.singh7924@gmail.com>
2020-05-17 10:08:14 +0000

    upgrage vibrator hal vwrsion to 1.3
    Change-Id: Iadf9fad3194b072f8f9b24557ee9adf4a527a252

--- hardware/interface/vibrator/Vibrator.cpp
+++ hardware/interface/vibrator/Vibrator.cpp
@@ -15,15 +15,14 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vibrator@1.0-exynos7904"
+#define LOG_TAG "vibrator@1.3-samsung"
+
+#include "Vibrator.h"
 
 #include <android-base/logging.h>
 #include <android-base/stringprintf.h>
-
 #include <hardware/hardware.h>
 #include <hardware/vibrator.h>
-
-#include "Vibrator.h"
 
 #include <cinttypes>
 #include <cmath>
@@ -37,21 +36,19 @@
 #define CLICK_TIMING_MS 20
 
 #define VIBRATOR_TIMEOUT_PATH "/sys/class/timed_output/vibrator/enable"
-#define VIBRATOR_HAPTIC_PATH "/sys/class/timed_output/vibrator/haptic_engine"
 #define VIBRATOR_INTENSITY_PATH "/sys/class/timed_output/vibrator/intensity"
 
 namespace android {
 namespace hardware {
 namespace vibrator {
-namespace V1_0 {
+namespace V1_3 {
 namespace implementation {
 
 /*
  * Write value to path and close file.
  */
 template <typename T>
-static Return<Status> writeNode(const std::string& path, const T& value)
-{
+static Return<Status> writeNode(const std::string& path, const T& value) {
     std::ofstream node(path);
     if (!node) {
         LOG(ERROR) << "Failed to open: " << path;
@@ -69,48 +66,40 @@ static Return<Status> writeNode(const std::string& path, const T& value)
     return Status::OK;
 }
 
-static bool doesNodeExist(const std::string& path)
-{
+static bool doesNodeExist(const std::string& path) {
     std::ifstream f(path.c_str());
     return f.good();
 }
 
-Vibrator::Vibrator()
-{
+Vibrator::Vibrator() {
     bool ok;
 
     ok = doesNodeExist(VIBRATOR_TIMEOUT_PATH);
     if (ok) {
         mIsTimedOutVibriator = true;
     }
-}
 
-Status Vibrator::activate(uint32_t timeoutMs)
-{
-    if (!mIsTimedOutVibriator) {
-        return Status::UNSUPPORTED_OPERATION;
+    ok = doesNodeExist(VIBRATOR_INTENSITY_PATH);
+    if (ok) {
+        mhasTimedOutIntensity = true;
     }
-
-    return writeNode(VIBRATOR_TIMEOUT_PATH, timeoutMs);
 }
 
-Return<Status> Vibrator::on(uint32_t timeoutMs)
-{
+// Methods from ::android::hardware::vibrator::V1_0::IVibrator follow.
+
+Return<Status> Vibrator::on(uint32_t timeoutMs) {
     return activate(timeoutMs);
 }
 
-Return<Status> Vibrator::off()
-{
+Return<Status> Vibrator::off() {
     return activate(0);
 }
 
-Return<bool> Vibrator::supportsAmplitudeControl()
-{
+Return<bool> Vibrator::supportsAmplitudeControl() {
     return true;
 }
 
-Return<Status> Vibrator::setAmplitude(uint8_t amplitude)
-{
+Return<Status> Vibrator::setAmplitude(uint8_t amplitude) {
     uint32_t intensity;
 
     if (amplitude == 0) {
@@ -125,74 +114,62 @@ Return<Status> Vibrator::setAmplitude(uint8_t amplitude)
     }
     LOG(DEBUG) << "setting intensity: " << intensity;
 
-    if (doesNodeExist(VIBRATOR_INTENSITY_PATH)) {
+    if (mhasTimedOutIntensity) {
         return writeNode(VIBRATOR_INTENSITY_PATH, intensity);
-    }
-
-    if (doesNodeExist(VIBRATOR_HAPTIC_PATH)) {
-        std::string haptic = android::base::StringPrintf("4 %u %u %u %u",
-                                                         CLICK_TIMING_MS,
-                                                         intensity,
-                                                         2000,
-                                                         1);
-
-        return writeNode(VIBRATOR_HAPTIC_PATH, haptic);
     }
 
     return Status::OK;
 }
 
-uint8_t Vibrator::strengthToAmplitude(EffectStrength strength, Status* status)
-{
-    *status = Status::OK;
+Return<void> Vibrator::perform(V1_0::Effect effect, EffectStrength strength, perform_cb _hidl_cb) {
+    return perform<decltype(effect)>(effect, strength, _hidl_cb);
+}
 
-    switch (strength) {
-    case EffectStrength::LIGHT:
-        return 78;
-    case EffectStrength::MEDIUM:
-        return 128;
-    case EffectStrength::STRONG:
-        return 204;
+// Methods from ::android::hardware::vibrator::V1_1::IVibrator follow.
+
+Return<void> Vibrator::perform_1_1(V1_1::Effect_1_1 effect, EffectStrength strength,
+                                   perform_cb _hidl_cb) {
+    return perform<decltype(effect)>(effect, strength, _hidl_cb);
+}
+
+// Methods from ::android::hardware::vibrator::V1_2::IVibrator follow.
+
+Return<void> Vibrator::perform_1_2(V1_2::Effect effect, EffectStrength strength,
+                                   perform_cb _hidl_cb) {
+    return perform<decltype(effect)>(effect, strength, _hidl_cb);
+}
+
+// Methods from ::android::hardware::vibrator::V1_3::IVibrator follow.
+
+Return<bool> Vibrator::supportsExternalControl() {
+    return true;
+}
+
+Return<Status> Vibrator::setExternalControl(bool enabled) {
+    if (mEnabled) {
+        LOG(WARNING) << "Setting external control while the vibrator is enabled is "
+                        "unsupported!";
+        return Status::UNSUPPORTED_OPERATION;
     }
 
-    *status = Status::UNSUPPORTED_OPERATION;
-    return 0;
+    LOG(INFO) << "ExternalControl: " << mExternalControl << " -> " << enabled;
+    mExternalControl = enabled;
+    return Status::OK;
 }
 
-uint32_t Vibrator::effectToMs(Effect effect, Status* status) {
-    *status = Status::OK;
-
-    switch (effect) {
-    case Effect::CLICK:
-        return 20;
-    case Effect::DOUBLE_CLICK:
-        return 25;
-    }
-
-    *status = Status::UNSUPPORTED_OPERATION;
-    return 0;
+Return<void> Vibrator::perform_1_3(Effect effect, EffectStrength strength, perform_cb _hidl_cb) {
+    return perform<decltype(effect)>(effect, strength, _hidl_cb);
 }
 
-const std::string Vibrator::effectToName(Effect effect)
-{
-    return toString(effect);
-}
+// Private methods follow.
 
-const std::string Vibrator::effectStrengthToName(EffectStrength strength)
-{
-    return toString(strength);
-}
-
-Return<void> Vibrator::perform(Effect effect,
-                               EffectStrength strength,
-                               perform_cb _hidl_cb)
-{
+Return<void> Vibrator::perform(Effect effect, EffectStrength strength, perform_cb _hidl_cb) {
     Status status = Status::OK;
     uint8_t amplitude;
     uint32_t ms;
 
-    LOG(DEBUG) << "perform effect: " << effectToName(effect) <<
-                 ", strength: " << effectStrengthToName(strength);
+    LOG(DEBUG) << "perform effect: " << effectToName(effect)
+               << ", strength: " << effectStrengthToName(strength);
 
     amplitude = strengthToAmplitude(strength, &status);
     if (status != Status::OK) {
@@ -213,8 +190,88 @@ Return<void> Vibrator::perform(Effect effect,
     return Void();
 }
 
+template <typename T>
+Return<void> Vibrator::perform(T effect, EffectStrength strength, perform_cb _hidl_cb) {
+    auto validRange = hidl_enum_range<T>();
+    if (effect < *validRange.begin() || effect > *std::prev(validRange.end())) {
+        _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
+        return Void();
+    }
+    return perform(static_cast<Effect>(effect), strength, _hidl_cb);
+}
+
+Status Vibrator::activate(uint32_t timeoutMs) {
+    std::lock_guard<std::mutex> lock{mMutex};
+    if (!mIsTimedOutVibriator) {
+        return Status::UNSUPPORTED_OPERATION;
+    }
+
+    return writeNode(VIBRATOR_TIMEOUT_PATH, timeoutMs);
+}
+
+uint8_t Vibrator::strengthToAmplitude(EffectStrength strength, Status* status) {
+    *status = Status::OK;
+
+    switch (strength) {
+        case EffectStrength::LIGHT:
+            return 78;
+        case EffectStrength::MEDIUM:
+            return 128;
+        case EffectStrength::STRONG:
+            return 204;
+    }
+
+    *status = Status::UNSUPPORTED_OPERATION;
+    return 0;
+}
+
+uint32_t Vibrator::effectToMs(Effect effect, Status* status) {
+    *status = Status::OK;
+
+    switch (effect) {
+        case Effect::CLICK:
+            return 20;
+        case Effect::DOUBLE_CLICK:
+            return 25;
+        case Effect::HEAVY_CLICK:
+            return 30;
+        case Effect::TICK:
+        case Effect::TEXTURE_TICK:
+        case Effect::THUD:
+        case Effect::POP:
+            return 15;
+        case Effect::RINGTONE_1:
+        case Effect::RINGTONE_2:
+        case Effect::RINGTONE_3:
+        case Effect::RINGTONE_4:
+        case Effect::RINGTONE_5:
+        case Effect::RINGTONE_6:
+        case Effect::RINGTONE_7:
+        case Effect::RINGTONE_8:
+        case Effect::RINGTONE_9:
+        case Effect::RINGTONE_10:
+        case Effect::RINGTONE_11:
+        case Effect::RINGTONE_12:
+        case Effect::RINGTONE_13:
+        case Effect::RINGTONE_14:
+        case Effect::RINGTONE_15:
+            return 300;
+    }
+
+    *status = Status::UNSUPPORTED_OPERATION;
+    return 0;
+}
+
+const std::string Vibrator::effectToName(Effect effect) {
+    return toString(effect);
+}
+
+const std::string Vibrator::effectStrengthToName(EffectStrength strength) {
+    return toString(strength);
+}
+
 }  // namespace implementation
-}  // namespace V1_0
+}  // namespace V1_3
 }  // namespace vibrator
 }  // namespace hardware
 }  // namespace android
